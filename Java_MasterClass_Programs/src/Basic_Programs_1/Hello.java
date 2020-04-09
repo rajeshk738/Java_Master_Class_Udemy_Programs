d2f77a55b61b83299254d942ff621ce68b659165
Rajesh <Rajesh@DESKTOP-OMQBV2S>
2020-04-09 13:04:44 +0530

    test

---
+++ Java_MasterClass_Programs/src/Basic_Programs_1/Hello.java
@@ -0,0 +1,15 @@
+package Basic_Programs_1;
+//hello world program
+
+ 
+public class Hello{
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");	
+		
+		int myFirstNumber =5+10;
+		System.out.println(myFirstNumber);
+
+	}
+	
+}
