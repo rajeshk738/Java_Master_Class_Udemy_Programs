18012ff9ee72de6e706a5620b25c45d2b96ce89d
Rajesh <Rajesh@DESKTOP-OMQBV2S>
2020-04-09 13:05:06 +0530

    update

---
+++ Java_MasterClass_Programs/src/Basic_Programs_1/Expressions.java
@@ -0,0 +1,22 @@
+package Basic_Programs_1;
+
+/**
+ * @author Rajesh
+ *
+ */
+public class Expressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myFirstNumber =  (10+5) + (2*10);
+		int mySecondNumber = 12;
+		int myThirdNumber = 6;
+		int myTotalNumber = myFirstNumber + mySecondNumber +myThirdNumber;
+		System.out.println(myFirstNumber);
+		System.out.println(myTotalNumber);
+		
+		
+	}
+
+}
---
+++ src/Primitive_Challenge.java
@@ -0,0 +1,17 @@
+
+public class Primitive_Challenge {
+
+	public static void main(String[] args) {
+
+		byte b = 125;
+		short s = 150;
+		int i = 40000;
+		long  l = 50000L + 10L *(b+s+i);
+		System.out.println(l);
+		
+		short shortTotal = (short) (1000 + 10 * (b+s+i));  // --> casting is required from int to short	
+		System.out.println(shortTotal);
+
+	}
+
+}
